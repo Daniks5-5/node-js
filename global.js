fd0873a9a7accc6ca15a5f6d391152b4574c89f3
Danik <elik@example.com>
2024-01-04 17:16:44 +0300

    info

---
+++ global.js
@@ -0,0 +1,16 @@
+//console.log(global);
+
+// setTimeout(() => {
+//     console.log('Hello!');
+// },1000);
+
+// console.log(__dirname);
+
+// console.log(__filename);
+console.log(process);
+console.log(process.argv);
+
+const url = new URL('https://nodejs.org/api/globals.html');
+console.log(url.hostname);
+console.log(url.protocol);
+console.log(url.href);
---
+++ test.js
@@ -0,0 +1,11 @@
+const userName = "Ivan";
+
+const sayHi = (userName) => `Hello! My name is ${userName}`;
+
+console.log(sayHi(userName));
+
+const userFamily = "Sidorov";
+
+const sayFam = (userFamily) => `My lastname is ${userFamily}`;
+
+console.log(sayFam(userFamily));
