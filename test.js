6c9bc07700fc780f004f191a7faf1541f7f3d816
Danik <elik@example.com>
2024-01-05 10:54:11 +0300

    modules

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ const userName = "Ivan";
 
 const sayHi = (userName) => `Hello! My name is ${userName}`;
 
-console.log(sayHi(userName));
-
 const userFamily = "Sidorov";
 
 const sayFam = (userFamily) => `My lastname is ${userFamily}`;
 
-console.log(sayFam(userFamily));
+module.exports = {
+    userName,
+    sayHi,
+    userFamily,
+    sayFam,
+
+}
