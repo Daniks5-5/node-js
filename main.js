77713102e05059a8004aba66466e72dd0ff65584
Danil <you@gmail.com>
2024-01-06 13:07:54 +0300

    test make file

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const {number, userName} = require('./ex1');
 console.log(number(1,20));
 
+const fs = require('fs');
+
+fs.readFile('./estima1.txt', 'utf8',(error,data) =>{
+
+    console.log(data);
+    fs.writeFile('./estima2.txt', data, () => {
+
+    } );
+
+});
